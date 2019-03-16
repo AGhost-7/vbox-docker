ab7c825a18e6ba97f4c669d80ec0c5acb80625b0
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-03-16 19:27:08 -0400

    Just need to figure out what is going on with ssh...

---
+++ README.md
@@ -0,0 +1,5 @@
+# Virtualbox Docker
+Minimal Alpine virtual machine for running docker on non-linux hosts.
+
+## Motivation
+Performance.
--- packer/upload.py
+++ packer/upload.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from os import environ
 import requests
 from datetime import datetime
 
@@ -11,11 +10,11 @@ def fetch_build_number():
     request = requests.get(BASE_URL + '/releases/latest')
     body = request.json()
     tag_name = body['tag_name']
-    return tag_name.split('+')[1]
+    return int(tag_name.split('+')[1]) + 1
 
 
 def create_release(build_number):
-    version = 'v{:%d.%m.%y}+{}'.format(datetime.now(), build_number)
+    version = 'v{:%y.%m.%d}+{}'.format(datetime.now(), build_number)
     payload = {
         'tag_name': version,
         'name': 'Release {}'.format(version)
@@ -27,9 +26,14 @@ def create_release(build_number):
 
 
 def upload_artifacts(release):
+    upload_url = release['upload_url'].split('{')[0]
+    print('upload url: {}', upload_url)
     pass
 
 
 build_number = fetch_build_number()
+print('New build number is {}'.format(build_number))
 release = create_release(build_number)
+print('Created release {}', release['tag_name'])
+print('Uploading artifacts...')
 upload_artifacts(release)

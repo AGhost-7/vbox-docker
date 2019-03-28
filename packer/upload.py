cd122c24b4915219721cbd74c7154f9205548e0f
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-03-27 21:06:17 -0400

    finish upload script

--- packer/upload.py
+++ packer/upload.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import requests
+from os import environ, listdir, path
+
 from datetime import datetime
 
 BASE_URL = 'https://api.github.com/repos/AGhost-7/vbox-docker'
@@ -19,14 +21,34 @@ def create_release(build_number):
         'tag_name': version,
         'name': 'Release {}'.format(version)
     }
+    headers = {'authorization': 'Bearer ' + environ['GH_TOKEN']}
+    response = requests.post(
+        BASE_URL + '/releases', json=payload, headers=headers)
+    assert response.status_code < 300
+    return response.json()
 
-    request = requests.post(BASE_URL + '/releases', data=payload)
-    assert request.status_code < 300
-    return request.json()
+
+def find_minimal():
+    for file_name in listdir('./output/minimal'):
+        _, extension = path.splitext(file_name)
+        print('extension: {}'.format(extension))
+        if extension == '.vmdk':
+            return open(path.join('./output/minimal', file_name), 'rb')
+
+    raise Exception('Could not find minimal vm image to upload')
 
 
 def upload_artifacts(release):
     upload_url = release['upload_url'].split('{')[0]
+    headers = {
+        'authorization': 'Bearer ' + environ['GH_TOKEN'],
+        'content-type': 'application/octet-stream'
+    }
+    response = requests.post(
+        upload_url + '?name=vbox-docker.vmdk',
+        data=find_minimal(),
+        headers=headers)
+    assert response.status_code < 300
     print('upload url: {}', upload_url)
     pass
 

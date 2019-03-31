a9eb53f07119ca31a637c37294b18b15824a5180
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-03-31 12:27:57 -0400

    Add ssh user parameter

--- vbox-docker.sh
+++ vbox-docker.sh
@@ -2,18 +2,24 @@
 
 set -eo pipefail
 
+export ANSIBLE_HOST_KEY_CHECKING=false
+
 script_source="${BASH_SOURCE[@]}"
 if [ -L "$script_source" ]; then
 	script_source="$(readlink "$script_source")"
 fi
 cd "$(dirname "$script_source")"
 
+ssh_user=vbox
 vm_ssh() {
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .data/key -p 2223 vbox@localhost $@
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .data/key -p 2223 "$ssh_user@localhost" $@
 }
 
 case "$1" in
 	ssh)
+		if [ "$2" ]; then
+			ssh_user="$2"
+		fi
 		vm_ssh
 		;;
 	login)
